3fe77e4877865a828c8b91f7dc51e2ce89af8847
Jesse Jurman <j.r.jurman@gmail.com>
2015-02-12 18:53:04 -0500

    fixing bugs?

--- src/chordmap.js
+++ src/chordmap.js
@@ -39,18 +39,18 @@ var keys = ["C", "C#", "Db", "D", "D#", "Eb",
  "A", "A#", "Bb", "B"];
 
 keys.forEach( function(e) {
-   chordmap[e+" major"] = getFingering(note, [0, 4, 7]);
-   chordmap[e+" minor"] = getFingering(note, [0, 3, 7]);
-   chordmap[e+" 7"] = getFingering(note, [0, 4, 7, 10]);
-   chordmap[e+" m7"] = getFingering(note, [0, 3, 7, 10]);
-   chordmap[e+" maj7"] = getFingering(note, [0, 4, 7, 11]);
-   chordmap[e+" mM7"] = getFingering(note, [0, 3, 7, 11]);
-   chordmap[e+" 7b5"] = getFingering(note, [0, 4, 6, 10]);
-   chordmap[e+" 7#5"] = getFingering(note, [0, 4, 8, 10]);
-   chordmap[e+" 7b9"] = getFingering(note, [0, 4, 7, 10, 13]);
-   chordmap[e+" 7#9"] = getFingering(note, [0, 4, 7, 10, 15]);
-   chordmap[e+" b5"] = getFingering(note, [0, 4, 6]);
-   chordmap[e+" 5"] = getFingering(note, [0, 7, 12]);
+   chordmap[e+" major"] = getFingering(e, [0, 4, 7]);
+   chordmap[e+" minor"] = getFingering(e, [0, 3, 7]);
+   chordmap[e+" 7"] = getFingering(e, [0, 4, 7, 10]);
+   chordmap[e+" m7"] = getFingering(e, [0, 3, 7, 10]);
+   chordmap[e+" maj7"] = getFingering(e, [0, 4, 7, 11]);
+   chordmap[e+" mM7"] = getFingering(e, [0, 3, 7, 11]);
+   chordmap[e+" 7b5"] = getFingering(e, [0, 4, 6, 10]);
+   chordmap[e+" 7#5"] = getFingering(e, [0, 4, 8, 10]);
+   chordmap[e+" 7b9"] = getFingering(e, [0, 4, 7, 10, 13]);
+   chordmap[e+" 7#9"] = getFingering(e, [0, 4, 7, 10, 15]);
+   chordmap[e+" b5"] = getFingering(e, [0, 4, 6]);
+   chordmap[e+" 5"] = getFingering(e, [0, 7, 12]);
 
-   chordmap[e+" dim"] = getFingering(note, [0, 3, 6]);
+   chordmap[e+" dim"] = getFingering(e, [0, 3, 6]);
 });
